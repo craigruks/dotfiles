d045f386d07db5a06b320140e27f68525419c750
Craig Ruks <craig@craigruks.com>
2016-05-26 22:22:40 +0700

    Updated brew, cask with apps that I actively use

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,51 +1,94 @@
 #!/bin/bash
 
+# to maintain cask: `brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+# to search for new ones https://caskroom.github.io/search
 
-# to maintain cask ....
-#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
 
-
-# Install native apps
-
+# cask itself
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
-# daily
-brew cask install spectacle
+# browsers
+brew cask install firefox
+brew cask install google-chrome
+
+# cloud
 brew cask install dropbox
-brew cask install gyazo
+brew cask install google-drive
+
+# daily
 brew cask install 1password
-brew cask install rescuetime
-brew cask install flux
+brew cask install alfred
+brew cask install spectacle
 
 # dev
-brew cask install iterm2
-brew cask install sublime-text
-brew cask install imagealpha
+brew cask install adobe-creative-cloud
+brew cask install adobe-illustrator-cc
+brew cask install adobe-indesign-cc
+brew cask install adobe-photoshop-cc
+brew cask install adobe-photoshop-lightroom
+brew cask install atom
+brew cask install gifrocket
+brew cask install gitbox
+brew cask install gitup
 brew cask install imageoptim
+brew cask install iterm2
+brew cask install openoffice
+brew cask install postgres
+brew cask install postico
+brew cask install qlmarkdown
+brew cask install quiver
+brew cask install rested
+brew cask install shuttle
+brew cask install sublime-text
+brew cask install transmit
+brew cask install virtualbox
 
-# fun
-brew cask install limechat
-brew cask install miro-video-converter
-brew cask install horndis               # usb tethering
+# maintenance
+brew cask install apptrap
+brew cask install carbon-copy-cloner
+brew cask install cleanmymac
+# brew cask install coconutbattery  # check iphone battery
+brew cask install daisydisk
 
-# browsers
-brew cask install google-chrome-canary
-brew cask install firefoxnightly
-brew cask install webkit-nightly
-brew cask install chromium
-brew cask install torbrowser
-
-# less often
-brew cask install disk-inventory-x
-brew cask install screenflow
+# media
+brew cask install flash-player  # for spotify
+brew cask install keepingyouawake
+brew cask install lastfm
+brew cask install plex-media-server
+brew cask install spotify
 brew cask install vlc
-brew cask install gpgtools
-brew cask install licecap
+
+# security
+brew cask install knockknock
+brew cask install little-snitch
+brew cask install lockdown  # https://github.com/SummitRoute/osxlockdown
+brew cask install malwarebytes-anti-malware
+
+# social
+brew cask install franz
+brew cask install skype
+brew cask install slack
+
+# usenet
+brew cask install couchpotato
+brew cask install sabnzbd
+brew cask install sonarr
 brew cask install utorrent
+brew cask install unrarx
+brew cask install viscosity
 
 
 # Not on cask but I want regardless.
 
-# File Multi Tool 5
-# Phosphor
+# airmail 2
+# day one
+# gemini 2
+# docker beta
+# feedly
+# gotomeeting
+# icon slate
+# monity
+# monosnap
+# power json editor
+# wunderlist
--- brew.sh
+++ brew.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Install command-line tools using Homebrew
-
 # Make sure we’re using the latest Homebrew
 brew update
 
 # Upgrade any already-installed formulae
 brew upgrade
+
 
 
 # GNU core utilities (those that come with OS X are outdated)
@@ -15,65 +14,51 @@ brew install moreutils
 # GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
 brew install findutils
 # GNU `sed`, overwriting the built-in `sed`
-brew install gnu-sed --default-names
+brew install gnu-sed --with-default-names
 
-
-# Bash 4
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+# Bash 4 + autocompletion
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
-
 brew install homebrew/completions/brew-cask-completion
 
-# generic colouriser  http://kassiopeia.juls.savba.sk/~garabik/software/grc/
+# generic colouriser http://kassiopeia.juls.savba.sk/~garabik/software/grc/
 brew install grc
-
-# Install wget with IRI support
-brew install wget --with-iri
 
 # Install more recent versions of some OS X tools
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-
-
-# run this script when this file changes guy.
-brew install entr
-
-# github util. imho better than hub
-brew install gh
-
-
-# mtr - ping & traceroute. best.
-brew install mtr
-
-    # allow mtr to run without sudo
-    mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
-    sudo chmod 4755 $mtrlocation/sbin/mtr
-    sudo chown root $mtrlocation/sbin/mtr
-
-
-# Install other useful binaries
-brew install the_silver_searcher
-brew install fzf
-
 brew install git
+
+# python
+brew install python
+brew install python3
 brew install imagemagick --with-webp
+
+# ruby via rvm, finished in setup-a-new-machine.sh
+brew install gpg
+
+# heroku development
+brew install heroku-toolbelt
+
+# https://github.com/sivel/speedtest-cli
+brew install speedtest_cli
+
+# other useful binaries
 brew install node # This installs `npm` too using the recommended installation method
-brew install pv
-brew install rename
+
+# tree of current dir
 brew install tree
+
+# compress file gzip zlib
 brew install zopfli
+
+# video encoder
 brew install ffmpeg --with-libvpx
 
-brew install terminal-notifier
 
-brew install android-platform-tools
-brew install pidcat   # colored logcat guy
-
-brew install zsh
 
 # Remove outdated versions from the cellar
 brew cleanup
